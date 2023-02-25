8732cf31e4b2d5ab17cebc3115294f90b3b56dd4
Alejandro Gómez <aldajo92mac@AlejandroMacM1.local>
2023-02-25 17:24:15 -0500

    removed commented lines

--- app/src/main/java/com/example/androidanimationscompose/MainActivity.kt
+++ app/src/main/java/com/example/androidanimationscompose/MainActivity.kt
@@ -6,15 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.InfiniteRepeatableSpec
 import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.TweenSpec
 import androidx.compose.animation.core.VectorConverter
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -36,6 +33,7 @@ import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androidanimationscompose.ui.theme.AndroidAnimationsComposeTheme
+import kotlin.math.pow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,7 +45,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    XYAxisBoard(modifier = Modifier.fillMaxSize())
+                    XYAxisBoard(modifier = Modifier.fillMaxSize().background(Color.White))
                     AnimatedCircleV2(modifier = Modifier.fillMaxSize())
                 }
             }
@@ -55,57 +53,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
-@Composable
-fun AnimatedCircle() {
-    val xPosition by remember { mutableStateOf(0f) }
-    val yPosition by remember { mutableStateOf(0f) }
-
-    var width by remember { mutableStateOf(0f) }
-    var height by remember { mutableStateOf(0f) }
-
-    Box(
-        Modifier
-            .fillMaxSize()
-            .layout { measurable, constraints ->
-                val placeable = measurable.measure(constraints)
-                layout(placeable.width, placeable.height) {
-                    width = placeable.width.toFloat()
-                    height = placeable.height.toFloat()
-                    placeable.placeRelative(0, 0)
-                }
-            }
-    ) {
-        val position = remember { Animatable(Offset(xPosition, yPosition), Offset.VectorConverter) }
-        LaunchedEffect(Unit) {
-            position.animateTo(
-                targetValue = Offset(width, height),
-                animationSpec = tween(durationMillis = 5000, easing = LinearEasing)
-            )
-        }
-        Canvas(modifier = Modifier.fillMaxSize()) {
-            drawCircle(
-                color = Color.Blue,
-                radius = size.minDimension / 20,
-                center = position.value,
-                style = Stroke(5f)
-            )
-        }
-    }
-}
 
 @Preview
 @Composable
 fun AnimatedCircleV2(modifier: Modifier = Modifier) {
-//    val tValue by remember { mutableStateOf(-3f) }
-//    val tAnimation = remember { Animatable(tValue, Float.VectorConverter) }
-//    LaunchedEffect(Unit) {
-//        tAnimation.animateTo(
-//            targetValue = 5f,
-//            animationSpec = tween(durationMillis = 5000, easing = LinearEasing)
-//        )
-//    }
-
     var isRunning by remember { mutableStateOf(false) }
 
     var width by remember { mutableStateOf(0f) }
@@ -116,7 +67,7 @@ fun AnimatedCircleV2(modifier: Modifier = Modifier) {
         PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
     }
 
-    val tParameter by remember { mutableStateOf(-3f) }
+    var tParameter by remember { mutableStateOf(-3f) }
     val tAnimation2 = remember { Animatable(tParameter, Float.VectorConverter) }
     val animationSpec = remember {
         InfiniteRepeatableSpec<Float>(
@@ -124,12 +75,15 @@ fun AnimatedCircleV2(modifier: Modifier = Modifier) {
         )
     }
     LaunchedEffect(isRunning) {
-        if (isRunning and !tAnimation2.isRunning) {
+        if (isRunning) {
+            tAnimation2.snapTo(-3f)
             tAnimation2.animateTo(
                 targetValue = 5f,
                 animationSpec = animationSpec
-            )
-        } else tAnimation2.stop()
+            ) {
+                tParameter = this.value
+            }
+        }
     }
 
     Box(
@@ -143,14 +97,17 @@ fun AnimatedCircleV2(modifier: Modifier = Modifier) {
                 }
             }
     ) {
-        val origin = Point(width, height)
-        val newOrigin = origin / 2f
+        val screenBottomRightCorner = Point(width, height)
+        val newOrigin = screenBottomRightCorner / 2f
 
         val circleInitialCenter = Point(0f, 200f)
-        val pDirector = Point(100f, -100f)
+        val pDirector = Point(-100f, 100f)
 
-        val circleCenter = rectLine(circleInitialCenter, pDirector, tAnimation2.value)
+        val circleCenter = rectLine(circleInitialCenter, pDirector, tParameter)
             .invertYaxis().translate(newOrigin)
+
+//        val circleCenter = parabola(tParameter)
+//            .invertYaxis().translate(newOrigin)
 
         Canvas(modifier = Modifier.fillMaxSize()) {
             drawCircle(
@@ -174,7 +131,7 @@ fun AnimatedCircleV2(modifier: Modifier = Modifier) {
         }
         Text(
             modifier = Modifier.padding(10.dp),
-            text = "t: ${String.format("%.2f", tAnimation2.value)}"
+            text = "t: ${String.format("%.2f", tParameter)}"
         )
         Button(
             modifier = Modifier
@@ -191,3 +148,4 @@ fun AnimatedCircleV2(modifier: Modifier = Modifier) {
 
 
 fun rectLine(p0: Point, pointDirector: Point, t: Float): Point = p0 + (pointDirector * t)
+fun parabola(t: Float): Point = Point(t*80f, (t*10f).pow(2))
