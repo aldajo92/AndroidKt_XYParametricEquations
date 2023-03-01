8e84f0a15e15257488df6c7a2629db65eee9cbde
Alejandro Gomez <alejodiscovery20@gmail.com>
2023-03-01 00:16:29 -0500

    fix issue with keyboard that covers input fields

--- app/src/main/java/com/aldajo92/xyparametricequations/ui/SimpleContinuousSlider.kt
+++ app/src/main/java/com/aldajo92/xyparametricequations/ui/SimpleContinuousSlider.kt
@@ -4,10 +4,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Slider
 import androidx.compose.material.SliderDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
