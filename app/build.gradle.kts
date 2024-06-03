0c77d7bbbb78c74d211a4a2dd5aefbdfed214b26
Alejandro Gomez <alejodiscovery20@gmail.com>
2024-06-03 00:55:39 -0500

    Release 1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 
         minSdk = 24
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.6"
+        versionCode = 12
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
